ec15775c5e60c7309b671ff08a54b7845eec5bb6
Kyrene <Kyrene.Aflores@gmail.com>
2023-10-02 18:19:37 -0500

    finished signup user

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -35,43 +35,28 @@ router.post(
   '/',
   validateSignup,
   async (req, res) => {
-    const { email, password, username } = req.body;
+    const { email, password, username,firstName,lastName } = req.body;
     const hashedPassword = bcrypt.hashSync(password);
-    const user = await User.create({ email, username, hashedPassword });
+    const user = await User.create({ email, username, hashedPassword,firstName,lastName });
 
     const safeUser = {
       id: user.id,
+      firstName:user.firstName,
+      lastName:user.lastName,
       email: user.email,
       username: user.username,
+
     };
 
     await setTokenCookie(res, safeUser);
 
     return res.json({
-      user: safeUser
+      user:safeUser
+
+
     });
   }
 );
 
-  // router.post(
-  //   '/',
-  //   validateSignup,
-  //   async (req, res) => {
-  //     const { email, password, username } = req.body;
-  //     const hashedPassword = bcrypt.hashSync(password);
-  //     const user = await User.create({ email, username, hashedPassword });
 
-  //     const safeUser = {
-  //       id: user.id,
-  //       email: user.email,
-  //       username: user.username,
-  //     };
-
-  //     await setTokenCookie(res, safeUser);
-
-  //     return res.json({
-  //       user: safeUser
-  //     });
-  //   }
-  // );
 module.exports = router;
