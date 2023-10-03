7b61b31afd993b3df8bebeb0de56288ea76166ae
Kyrene <Kyrene.Aflores@gmail.com>
2023-10-03 17:36:30 -0500

    created membership seed

--- backend/db/models/membership.js
+++ backend/db/models/membership.js
@@ -40,6 +40,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Membership',
+    defaultScope:{
+      attributes:{
+        exclude:['createdAt','updatedAt']
+      }
+    }
   });
   return Membership;
 };
