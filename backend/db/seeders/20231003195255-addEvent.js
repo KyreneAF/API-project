29b398e01e7668d1d84b97353cd4b028113f19d5
Kyrene <Kyrene.Aflores@gmail.com>
2023-10-03 15:34:37 -0500

    added event seed

--- backend/db/seeders/20231003195255-addEvent.js
+++ backend/db/seeders/20231003195255-addEvent.js
@@ -1,5 +1,5 @@
 'use strict';
-const {Event} = require('../models')
+const {Event,Group,venue} = require('../models')
 
 const events =[
   {
@@ -11,29 +11,76 @@ const events =[
     price: 5,
     startDate: new Date('2023-10-31 20:00:00'),
     endDate: new Date('2023-10-31 22:00:00')
-  }
+  },
+  {
+    venue:'*change me*',
+    groupName: 'Cheese Tasting',
+    name: 'First Cheese Meeting',
+    type: 'In person',
+    capacity: 20,
+    price: 25,
+    startDate: new Date('2023-10-14 20:00:00'),
+    endDate: new Date('2023-10-14 22:00:00')
+  },
+  {
+    venue:'*change me*',
+    groupName: 'Kitty social',
+    name: 'Meet and Greet',
+    type: 'Online',
+    capacity: 50,
+    price: 0,
+    startDate: new Date('2023-12-31 20:00:00'),
+    endDate: new Date('2023-12-31 22:00:00')
+  },
+  {
+    venue:'*change me*',
+    groupName: 'Hiking Team',
+    name: 'Spring Hike',
+    type: 'In person',
+    capacity: 10,
+    price: 10,
+    startDate: new Date('2024-4-1 20:00:00'),
+    endDate: new Date('2024-4-1 22:00:00')
+  },
+  {
+    venue:'*change me*',
+    groupName: 'Book Club',
+    name: 'Twilight Reading',
+    type: 'Online',
+    capacity: 15,
+    price: 10,
+    startDate: new Date('2024-4-1 20:00:00'),
+    endDate: new Date('2024-4-1 22:00:00')
+  },
+
 ]
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    for(let meets of events){
+      const {venue,groupName,...eventDetails} = meets
+      const foundVenue = await Venue.findOne({
+        where:{
+          address: venue
+        }
+      });
+      const foundGroup = await Group.findOne({
+        where:{
+          name:groupName
+        },
+      })
+    }
+    await Event.create({
+      venueId:foundVenue.id,
+      groupId:foundGroup.id,
+      ...eventDetails,
+    })
+
+
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Events',{name: events.name},{})
   }
 };
